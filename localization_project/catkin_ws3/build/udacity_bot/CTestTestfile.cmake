250e8f9e79bcabf70b2c0be00b619dd4d5ea22e0
Thomas Legleu <45805583+ThomasLegleu@users.noreply.github.com>
2019-11-12 09:45:37 -0800

    catkin_workspace

---
+++ localization_project/catkin_ws3/build/udacity_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/catkin_ws3/src/udacity_bot
+# Build directory: /home/robond/catkin_ws3/build/udacity_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
