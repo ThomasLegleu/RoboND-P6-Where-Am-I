8bfbf04fe2ee5bd390828e16b4dbe8eadb857cbc
Thomas Legleu <45805583+ThomasLegleu@users.noreply.github.com>
2019-11-12 09:51:02 -0800

    delete a few things

--- localization_project/catkin_ws2/build/udacity_bot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/catkin_ws2/src/udacity_bot
-# Build directory: /home/robond/catkin_ws2/build/udacity_bot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- localization_project/catkin_ws3/build/udacity_bot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/catkin_ws3/src/udacity_bot
-# Build directory: /home/robond/catkin_ws3/build/udacity_bot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
